f785a0785bf5796523dcae4dacc70c511b36a12b
nova <courtois1337@gmail.com>
2019-08-23 12:05:40 -0500

    create lyrics component

---
+++ src/components/lyrics/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.scss';
+
+export default function Lyrics() {
+  return (
+    <blockquote>
+    <pre className='lyrics'>{`
+when you were a child on the farm
+you saw him pin her by the weight of his arms
+so she left in the night when you were in bed
+and took your trust when she left
+
+you're already hurt
+im scared that if you put
+your trust in me i'll make it worse
+i'll hurt you
+
+i picked a flower from the stem
+and watched it die in my hands
+and i screamed, "oh my god
+i did it again, im destroying innocence
+
+so i can't stay
+my heart's too weak to think
+about the way you use to say
+i miss you`
+    }
+      
+    </pre>
+    </blockquote>
+  );
+};
