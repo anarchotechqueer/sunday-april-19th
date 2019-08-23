ecd980fd00401d12641f6f8e15cff1021924712b
nova <courtois1337@gmail.com>
2019-08-23 12:23:44 -0500

    add base next-date component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Lyrics from './components/lyrics';
+import NextDate from './components/next-date';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Lyrics />
-    </>
+      <NextDate />
+    </div>
   );
 }
 
---
+++ src/components/next-date/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss';
+
+export default function NextDate() {
+  return (
+    <div className="next-date">
+      <span className="next-date__accent">// </span>
+      the next sunday, april 19th is
+    </div>
+  );
+};
