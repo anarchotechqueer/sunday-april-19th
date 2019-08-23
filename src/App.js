54122cc80ef391ea9172080b309ac2a14a716e71
nova <courtois1337@gmail.com>
2019-08-23 12:57:36 -0500

    calculate actual next occurrence

--- src/App.js
+++ src/App.js
@@ -5,13 +5,64 @@ import './app.scss';
 import Lyrics from './components/lyrics';
 import NextDate from './components/next-date';
 
-function App() {
-  return (
-    <div className="app">
-      <Lyrics />
-      <NextDate />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nextOccurrence: null
+    };
+  }
+
+  calculateNextOccurrence = () => {
+    let foundDate = false;
+    const targetMonth = 3; // April
+    const targetDate = 19; // 19th
+    const targetDay = 0; // sunday
+    const today = new Date();
+    let targetYear = (targetMonth <= today.getMonth() && targetDate <= today.getDate()) ? today.getFullYear() : today.getFullYear() + 1;
+    
+    let target = new Date();
+    
+    while (!foundDate) {
+      target.setFullYear(targetYear, targetMonth, targetDate);
+      // document.write(target);
+      if (target.getDay() === 0) {
+        foundDate = true;
+      }
+      else {
+        ++targetYear;
+      }
+    }
+
+    let targetText = targetYear == today.getFullYear() ? 'this year!' : `in ${targetYear}`;
+
+    this.setState({
+      nextOccurrence: targetText
+    })
+
+  }
+
+  componentDidMount() {
+    this.calculateNextOccurrence();
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+
+        { this.state.nextOccurrence && (
+          <>
+            <Lyrics />
+            <NextDate {...this.state} />
+          </>
+          )
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/next-date/index.js
+++ src/components/next-date/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.scss';
 
-export default function NextDate() {
+export default function NextDate(props) {
   return (
     <div className="next-date">
       <span className="next-date__accent">// </span>
-      the next sunday, april 19th is
+      the next sunday, april 19th is {props.nextOccurrence}
     </div>
   );
 };
